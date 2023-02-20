ad5447fc8da91ae78619111104316e438daf9d09
15104902830 <2021004725@qq.com>
2023-02-20 20:41:28 +0800

    header

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="test">
+        <g-header>
+            header
+        </g-header>
+
+        <g-aside>
+            aside
+        </g-aside>
+        <g-main>
+            mian
+        </g-main>
+        
         <g-button>自定义按钮</g-button>
     </div>
   
